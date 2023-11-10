c90a021dac3c1b4b8b1e34b369de1f2b9b879015
Nick Niehaus <nniehaus@users.noreply.huggingface.co>
2023-11-10 15:30:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,13 @@ square_footage = st.selectbox("Square Footage", ["<1000 sqft", "1000-2000 sqft",
 if st.button('Find Property'):
     # Process the inputs and call the OpenAI API
     user_input = f"I'm considering buying an investment property in Breckenridge and Blue River, CO. My maximum price is {max_price}. I'm looking for these amenities: {', '.join(amenities)}. Bedrooms: {bedrooms}, Bathrooms: {bathrooms}, Square Footage: {square_footage}."
+    
     # Call the OpenAI API here with the user_input and get the response
-    response = "API response based on user input will be displayed here."
+    response = openai.Completion.create(
+      engine="text-davinci-003", # Replace with your preferred model
+      prompt=user_input,
+      max_tokens=150
+    )
 
-    st.write(response)
+    # Display the response from the API
+    st.write(response.choices[0].text.strip())
