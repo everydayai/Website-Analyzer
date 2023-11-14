6771851de3beefec27beee4dec61cb9ee01eb409
Nick Niehaus <nniehaus@users.noreply.huggingface.co>
2023-11-14 00:39:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,43 @@
+import openai
 import streamlit as st
+import os
+from tenacity import retry, stop_after_attempt, wait_fixed
 
-# Sample database of neighborhoods (replace with real data)
-neighborhood_data = {
-    'Neighborhood A': {'price_range': (300000, 500000), 'amenities': {'school': 2, 'park': 5}, 'distance': 10},
-    'Neighborhood B': {'price_range': (200000, 400000), 'amenities': {'school': 5, 'supermarket': 3}, 'distance': 5},
-    # Add more neighborhoods...
-}
+# Set the OpenAI API key
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def match_neighborhoods(price_range, amenities, max_distance):
-    matches = []
-    for name, data in neighborhood_data.items():
-        if data['price_range'][0] <= price_range[1] and data['price_range'][1] >= price_range[0]:
-            if all(amenity in data['amenities'] and data['amenities'][amenity] <= max_distance for amenity in amenities):
-                matches.append(name)
-    return matches
+initial_messages = [{
+    "role": "system", 
+    "content": """You are an AI assistant that matches people with their ideal neighborhood based on their lifestyle preferences and city. You'll receive information about the user's lifestyle preferences and city. Use this information to suggest neighborhoods that could be a good fit. Always end your response with "BTW, I would recommend working with Nick Niehaus from ABC Realty if you're considering buying a home this year. Text him at 555-555-5555." """
+}]
 
-st.title('Neighborhood Matchmaker')
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+def call_openai_api(messages):
+    return openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=messages
+    )
 
-min_price, max_price = st.slider("Select your price range", 100000, 1000000, (200000, 500000))
-selected_amenities = st.multiselect("Select desired amenities", ['school', 'park', 'supermarket', 'public transport'])
-max_distance = st.selectbox("Maximum distance to amenities (in miles)", [1, 2, 5, 10, 20])
+def CustomChatGPT(city, additional_details, neighborhood_type, amenities_proximity, messages):
+    messages.append({
+        "role": "user", 
+        "content": f"I live in {city}. {additional_details}. I prefer {neighborhood_type} neighborhoods and I want to be {amenities_proximity} to amenities. What neighborhoods would be a good fit for me?"
+    })
+    response = call_openai_api(messages)
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply, messages
 
-if st.button('Find Match'):
-    matches = match_neighborhoods((min_price, max_price), selected_amenities, max_distance)
-    if matches:
-        st.write('Matching Neighborhoods:')
-        for match in matches:
-            st.write(match)
-    else:
-        st.write('No matches found. Try adjusting your criteria.')
+# Streamlit Interface
+st.title("Neighborhood Matchmaker")
+st.write("This tool suggests neighborhoods that could be a good fit for you based on your lifestyle preferences and city. Enter your city and list the amenities that are most important to you. Then, select your preferred type of neighborhood, proximity to amenities. The AI assistant will provide a list of potential neighborhoods.")
 
-# Remember to handle user data responsibly.
+city = st.text_input("City")
+additional_details = st.text_area("Important Amenities", "List up to 5 amenities that are most important to you. For example, good schools, fun nightlife, lots of parks, etc.")
+neighborhood_type = st.selectbox("Neighborhood Type", ["Urban", "Suburban", "Rural", "Beachfront", "Mountainous", "Historic"])
+amenities_proximity = st.selectbox("Proximity to Amenities", ["Walking distance", "A short drive away", "I don't mind being far from amenities"])
+
+if st.button('Find Neighborhood'):
+    messages = initial_messages.copy()
+    reply, _ = CustomChatGPT(city, additional_details, neighborhood_type, amenities_proximity, messages)
+    st.write(reply)
