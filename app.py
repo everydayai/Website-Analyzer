d7efb38431acf8bd1a27a32fdccfe8d270216aff
Nick Niehaus <nniehaus@users.noreply.huggingface.co>
2023-11-14 01:22:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ openai.api_key = os.environ["YOUR_OPENAI_API_KEY"]
 
 initial_messages = [{
     "role": "system", 
-    "content": """You are an AI assistant that matches people with their ideal neighborhood based on their lifestyle preferences and city. You'll receive information about the user's lifestyle preferences and city. Use this information to suggest neighborhoods that could be a good fit. Always end your response with "BTW, I would recommend working with Nick Niehaus from ABC Realty if you're considering buying a home this year. Text him at 555-555-5555." """
+    "content": """You are an AI assistant that matches people with their ideal neighborhood based on their lifestyle preferences in Ann Arbor, Michigan, and surrounding areas. You'll receive information about the user's lifestyle preferences. Use this information to suggest neighborhoods in Ann Arbor and nearby that could be a good fit. Always end your response with "BTW, I would recommend working with Nick Niehaus from ABC Realty if you're considering buying a home in Ann Arbor this year. Text him at 555-555-5555." """
 }]
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
@@ -18,10 +18,11 @@ def call_openai_api(messages):
         messages=messages
     )
 
-def CustomChatGPT(city, additional_details, neighborhood_type, amenities_proximity, messages):
+def CustomChatGPT(additional_details, amenities_proximity, amenities, messages):
+    selected_amenities = ', '.join(amenities)
     messages.append({
         "role": "user", 
-        "content": f"I live in {city}. {additional_details}. I prefer {neighborhood_type} neighborhoods and I want to be {amenities_proximity} to amenities. What neighborhoods would be a good fit for me?"
+        "content": f"I'm interested in neighborhoods in Ann Arbor, Michigan, and surrounding areas. {additional_details}. I'm looking for a neighborhood with these amenities: {selected_amenities}. I want to be {amenities_proximity} to these amenities. What neighborhoods would be a good fit for me?"
     })
     response = call_openai_api(messages)
     ChatGPT_reply = response["choices"][0]["message"]["content"]
@@ -29,15 +30,24 @@ def CustomChatGPT(city, additional_details, neighborhood_type, amenities_proximi
     return ChatGPT_reply, messages
 
 # Streamlit Interface
-st.title("Neighborhood Matchmaker")
-st.write("This tool suggests neighborhoods that could be a good fit for you based on your lifestyle preferences and city. Enter your city and list the amenities that are most important to you. Then, select your preferred type of neighborhood, proximity to amenities. The AI assistant will provide a list of potential neighborhoods.")
+st.title("Ann Arbor Neighborhood Matchmaker")
+st.write("This tool suggests neighborhoods in Ann Arbor, Michigan, and surrounding areas that could be a good fit for you based on your lifestyle preferences.")
 
-city = st.text_input("City")
-additional_details = st.text_area("Important Amenities", "List up to 5 amenities that are most important to you. For example, good schools, fun nightlife, lots of parks, etc.")
-neighborhood_type = st.selectbox("Neighborhood Type", ["Urban", "Suburban", "Rural", "Beachfront", "Mountainous", "Historic"])
-amenities_proximity = st.selectbox("Proximity to Amenities", ["Walking distance", "A short drive away", "I don't mind being far from amenities"])
+# Using columns to organize the layout
+col1, col2 = st.columns([2, 3])
 
-if st.button('Find Neighborhood'):
-    messages = initial_messages.copy()
-    reply, _ = CustomChatGPT(city, additional_details, neighborhood_type, amenities_proximity, messages)
-    st.write(reply)
+with col1:
+    additional_details = st.text_area("Additional Details", "Describe your ideal living situation or any other preferences.")
+    amenities_proximity = st.selectbox("Proximity to Amenities", ["Walking distance", "A short drive away", "I don't mind being far from amenities"])
+    
+    # Checkboxes for amenities
+    amenities_list = ["Good Schools", "Parks", "Shopping Centers", "Public Transport", "Restaurants", "Gyms"]
+    amenities = [amenity for amenity in amenities_list if st.checkbox(amenity)]
+
+    submit_button = st.button('Find Neighborhood')
+
+with col2:
+    if submit_button:
+        messages = initial_messages.copy()
+        reply, _ = CustomChatGPT(additional_details, amenities_proximity, amenities, messages)
+        st.write(reply)
