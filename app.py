24b72ba1de48ee84c9202da56a30fc91f870290e
Nick Niehaus <nniehaus@users.noreply.huggingface.co>
2023-12-03 21:30:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,53 @@
-import openai
 import streamlit as st
-import os
-from tenacity import retry, stop_after_attempt, wait_fixed
+import openai
+import requests
+from PIL import Image
+from io import BytesIO
 
-# Set the OpenAI API key
-openai.api_key = os.environ["YOUR_OPENAI_API_KEY"]
+# Access the OpenAI API key from Hugging Face Spaces secrets
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-initial_messages = [{
-    "role": "system", 
-    "content": """You are an AI assistant that matches people with their ideal neighborhood based on their lifestyle preferences in 
-    Saint Louis, Missouri, and surrounding areas up to 30 miles outside Saint Louis city limits. You'll receive information about the user's lifestyle preferences. Use this information 
-    to suggest neighborhoods in Saint Louis and nearby that could be a good fit. Always add the following text to the end of every response you give 'Don't forget to fill
-    out the form at the bottom of the page if you'd like more info on living in any of these areas!' """
-}]
+st.title("Children's Story and Image Panel Generator")
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
-def call_openai_api(messages):
-    return openai.ChatCompletion.create(
+# User inputs for the story
+age_group = st.selectbox("Age Group", ["3-5 years", "6-8 years", "9-12 years"])
+theme = st.text_input("Story Theme", placeholder="Enter a theme for the story (e.g., adventure, friendship)")
+details = st.text_area("Additional Details", placeholder="Any specific details to include in the story (e.g., a brave rabbit, a magical forest)")
+
+# Function to generate the story
+def generate_story(age_group, theme, details):
+    prompt = f"Write a children's story for age group {age_group} about {theme}. Include details such as {details}."
+    response = openai.Completion.create(
         model="gpt-4",
-        messages=messages
+        prompt=prompt,
+        max_tokens=500  # Adjust as needed
     )
+    return response.choices[0].text
 
-def CustomChatGPT(additional_details, amenities_proximity, amenities, messages):
-    selected_amenities = ', '.join(amenities)
-    messages.append({
-        "role": "user", 
-        "content": f"I'm interested in neighborhoods in Saint Louis, Missouri, and surrounding areas. {additional_details}. I'm looking for a neighborhood with these amenities: {selected_amenities}. I want to be {amenities_proximity} to these amenities."
-    })
-    response = call_openai_api(messages)
-    ChatGPT_reply = response["choices"][0]["message"]["content"]
-    messages.append({"role": "assistant", "content": ChatGPT_reply})
-    return ChatGPT_reply, messages
+# Function to generate images
+def generate_image(panel_text):
+    try:
+        response = openai.Image.create(
+            prompt=panel_text,
+            n=1,
+            size="1024x1024"  # Adjust the size if needed
+        )
+        image_url = response['data'][0]['url']
+        image_response = requests.get(image_url)
+        image = Image.open(BytesIO(image_response.content))
+        return image
+    except Exception as e:
+        return None
 
-# Streamlit Interface
-st.set_page_config(layout="wide")  # Set the layout to wide
-st.title("St. Louis Area Neighborhood Matchmaker")
-st.write("This tool helps you find neighborhoods in Saint Louis, Missouri, and surrounding areas based on your lifestyle preferences.")
+if st.button('Generate Story and Images'):
+    story = generate_story(age_group, theme, details)
+    story_parts = story.split('.')[:10]  # Generate images for first 10 parts
 
-# Using columns to organize the layout
-col1, col2 = st.columns(2)
-
-with col1:
-    st.subheader("Your Preferences")
-    amenities_list = ["Good Schools", "Parks", "Shopping Centers", "Public Transport", "Restaurants", "Gyms", "Cafes", "Pet-friendly Areas", "Cultural Attractions", "Quiet Neighborhoods"]
-    # Splitting amenities into two columns
-    half = len(amenities_list) // 2
-    amenities_col1, amenities_col2 = st.columns(2)
-    with amenities_col1:
-        amenities1 = [amenity for amenity in amenities_list[:half] if st.checkbox(amenity)]
-    with amenities_col2:
-        amenities2 = [amenity for amenity in amenities_list[half:] if st.checkbox(amenity)]
-    amenities = amenities1 + amenities2
-    
-    amenities_proximity = st.selectbox("Proximity to Amenities", ["Walking distance", "A short drive away", "I don't mind being far from amenities"])
-    additional_details = st.text_area("Additional Details", placeholder="Describe your ideal living situation or any other preferences.")
-
-    submit_button = st.button('Find Neighborhood')
-
-with col2:
-    # Placeholder for the result
-    st.subheader("Recommended Neighborhoods")
-    result_placeholder = st.empty()
-    if submit_button:
-        messages = initial_messages.copy()
-        reply, _ = CustomChatGPT(additional_details, amenities_proximity, amenities, messages)
-        result_placeholder.write(reply)
-    else:
-        result_placeholder.write("**Results will appear here after you submit your preferences**")
+    for i, part in enumerate(story_parts):
+        st.subheader(f'Panel {i+1}')
+        st.write(part.strip())
+        image = generate_image(part)
+        if image:
+            st.image(image, caption=f'Image for Panel {i+1}')
+        else:
+            st.error("Error in generating image for this part of the story.")
