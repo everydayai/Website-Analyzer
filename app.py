88073e5a958801973b1621397f4118b4ec156fd2
Nick Niehaus <nniehaus@users.noreply.huggingface.co>
2023-12-09 15:14:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,19 @@ import openai
 # Access the OpenAI API key from Hugging Face Spaces secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-st.title("AI Lead Magnet Recommender for Marketers")
+st.title("AI Lead Magnet Idea Generator for Marketers")
 
-# Collecting user input
-st.subheader("Tell Us About Your Business")
+# User inputs for the marketing plan
+st.subheader("Tell Us About Your Target Audience")
 business_type = st.text_input("Business Type", placeholder="e.g., Real Estate, E-commerce")
 target_audience = st.text_area("Target Audience", placeholder="Describe your primary customer base")
-current_marketing_strategies = st.text_area("Current Marketing Strategies", placeholder="Your current marketing efforts")
-business_goals = st.text_area("Business Goals and Challenges", placeholder="What do you aim to achieve with the lead magnet?")
 
-# Generate recommendations button
-if st.button('Generate AI Lead Magnet Recommendations'):
-    # Construct the prompt for the AI
+# Generate AI lead magnet ideas button
+if st.button('Generate AI Lead Magnet Ideas'):
+    # Construct the prompt for AI lead magnet ideas
     prompt_text = (
-        f"Based on the following details, recommend AI lead magnets suitable for engaging a customer base: "
-        f"Business type: {business_type}, target audience: {target_audience}, "
-        f"current marketing strategies: {current_marketing_strategies}, business goals: {business_goals}."
+        f"Generate ideas for AI-based lead magnets that a {business_type} company can offer to their target audience: {target_audience}. "
+        f"Focus on interactive and engaging AI-driven tools or content that can attract and provide value to their customers."
     )
 
     # Call the OpenAI API for text generation
@@ -27,16 +24,16 @@ if st.button('Generate AI Lead Magnet Recommendations'):
         response_text = openai.ChatCompletion.create(
             model="gpt-4",
             messages=[
-                {"role": "system", "content": "You are a marketing expert assistant."},
+                {"role": "system", "content": "You are an assistant who specializes in marketing and AI applications."},
                 {"role": "user", "content": prompt_text}
             ]
         )
-        recommendations = response_text.choices[0].message['content']
+        lead_magnet_ideas = response_text.choices[0].message['content']
     except Exception as e:
-        recommendations = f"Error in generating recommendations: {e}"
+        lead_magnet_ideas = f"Error in generating AI lead magnet ideas: {e}"
 
-    # Display the recommendations
-    st.markdown("### AI Lead Magnet Recommendations")
-    st.write(recommendations)
+    # Display the AI lead magnet ideas
+    st.markdown("### AI Lead Magnet Ideas")
+    st.write(lead_magnet_ideas)
 
 # Rest of your Streamlit code...
